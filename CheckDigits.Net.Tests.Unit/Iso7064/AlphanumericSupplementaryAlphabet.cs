f5879f14c23c0d4847028f32e899e247f8e103f1
mharges <michael.harges@gmail.com>
2023-10-23 15:08:56 -0700

    Iso7064PureSystemSingleCharacterAlgorithm updates

---
+++ CheckDigits.Net.Tests.Unit/Iso7064/AlphanumericSupplementaryAlphabet.cs
@@ -0,0 +1,41 @@
+﻿namespace CheckDigits.Net.Tests.Unit.Iso7064;
+
+public class AlphanumericSupplementaryAlphabet : IAlphabet
+{
+   private static readonly Int32[] _lookupTable =
+      Enumerable.Range(CharConstants.Asterisk, CharConstants.UpperCaseZ - CharConstants.Asterisk + 1)
+         .Select(x => x switch
+         {
+            CharConstants.Asterisk => 36,
+            Int32 d when d >= CharConstants.DigitZero && d <= CharConstants.DigitNine => d - CharConstants.DigitZero,
+            Int32 c when c >= CharConstants.UpperCaseA && c <= CharConstants.UpperCaseZ => c - CharConstants.UpperCaseA + 10,
+            _ => -1
+         }).ToArray();
+   private const Int32 _asteriskOffset = 0;
+   private const Int32 _digitLowerBound = CharConstants.DigitZero - CharConstants.Asterisk;
+   private const Int32 _digitUpperBound = CharConstants.DigitNine - CharConstants.Asterisk;
+   private const Int32 _alphaLowerBound = CharConstants.UpperCaseA - CharConstants.Asterisk;
+   private const Int32 _alphaUpperBound = CharConstants.UpperCaseZ - CharConstants.Asterisk;
+   private const String _checkCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ*";
+
+   public Int32 CharacterToInteger(Char ch)
+   {
+      var offset = ch - CharConstants.Asterisk;
+      return (offset >= _digitLowerBound && offset <= _digitUpperBound)
+            || (offset >= _alphaLowerBound && offset <= _alphaUpperBound)
+            ? _lookupTable[offset]
+            : -1;
+   }
+
+   public Int32 CheckCharacterToInteger(Char ch)
+   {
+      var offset = ch - CharConstants.Asterisk;
+      return offset == _asteriskOffset
+            || (offset >= _digitLowerBound && offset <= _digitUpperBound)
+            || (offset >= _alphaLowerBound && offset <= _alphaUpperBound)
+            ? _lookupTable[offset]
+            : -1;
+   }
+
+   public Char IntegerToCheckCharacter(Int32 checkDigit) => _checkCharacters[checkDigit];
+}
---
+++ CheckDigits.Net/Iso7064/IAlphabet.cs
@@ -0,0 +1,48 @@
+﻿namespace CheckDigits.Net.Iso7064;
+
+/// <summary>
+///   Defines the alphabet used by a generic ISO/IEC 7064 algorithm.
+/// </summary>
+public interface IAlphabet
+{
+   /// <summary>
+   ///   Maps a valid non-check digit character to its integer equivalent.
+   /// </summary>
+   /// <param name="ch">
+   ///   The character to convert.
+   /// </param>
+   /// <returns>
+   ///   The integer equivalent of <paramref name="ch"/> or -1 if 
+   ///   <paramref name="ch"/> is not a valid character.
+   /// </returns>
+   Int32 CharacterToInteger(Char ch);
+
+   /// <summary>
+   ///   Maps a valid check digit character to its integer equivalent.
+   /// </summary>
+   /// <param name="ch">
+   ///   The character to convert.
+   /// </param>
+   /// <returns>
+   ///   The integer equivalent of <paramref name="ch"/> or -1 if 
+   ///   <paramref name="ch"/> is not a valid character.
+   /// </returns>
+   /// <remarks>
+   ///   This method is only used by algorithms that require supplementary check
+   ///   characters, i.e. pure system algorithms that use a single check 
+   ///   character.
+   /// </remarks>
+   Int32 CheckCharacterToInteger(Char ch);
+
+   /// <summary>
+   ///   Maps the check digit calculated by an algorithm to its character 
+   ///   representation.
+   /// </summary>
+   /// <param name="checkDigit">
+   ///   The value to convert.
+   /// </param>
+   /// <returns>
+   ///   The character representation of the <paramref name="checkDigit"/> value.
+   /// </returns>
+   Char IntegerToCheckCharacter(Int32 checkDigit);
+}
