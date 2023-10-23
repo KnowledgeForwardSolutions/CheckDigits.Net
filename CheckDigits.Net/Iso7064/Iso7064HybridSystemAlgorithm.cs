2b85663e4a387843bf54dce53cb8ce3a401c24d3
mharges <michael.harges@gmail.com>
2023-10-23 16:53:37 -0700

    Iso7064HybridSystemAlgorithm updates

--- CheckDigits.Net/Iso7064/Iso7064HybridSystemAlgorithm.cs
+++ CheckDigits.Net/Iso7064/Iso7064HybridSystemAlgorithm.cs
@@ -1,43 +1,84 @@
 ﻿namespace CheckDigits.Net.Iso7064;
 
 /// <summary>
-///   Abstract base class for an ISO/IEC 7064 hybrid system algorithm (i.e. an 
-///   algorithm that uses two modulus values, M and M+1) and that generates a 
-///   single check character.
+///   Generic ISO/IEC 7064 hybrid system algorithm (i.e. an algorithm that uses 
+///   two modulus values, M and M+1) that generates a single check character.
 /// </summary>
-public class Iso7064HybridSystemAlgorithm
+public class Iso7064HybridSystemAlgorithm : ISingleCheckDigitAlgorithm
 {
+   private readonly IAlphabet _alphabet;
    private readonly Int32 _modulus;
    private readonly Int32 _modulusPlus1;
-   private readonly String _validCharacters;
 
+   /// <summary>
+   ///   Initialize a new <see cref="Iso7064HybridSystemAlgorithm"/>.
+   /// </summary>
+   /// <param name="algorithmName">
+   ///   The algorithm name.
+   /// </param>
+   /// <param name="algorithmDescription">
+   ///   Description of the algorithm.
+   /// </param>
+   /// <param name="modulus">
+   ///   The value used by the algorithm modulus operation.
+   /// </param>
+   /// <param name="alphabet">
+   ///   <see cref="IAlphabet"/> used to map characters to their integer 
+   ///   equivalents and vice versa.
+   /// </param>
+   /// <exception cref="ArgumentNullException">
+   ///   <paramref name="algorithmName"/> is <see langword="null"/>.
+   ///   - or -
+   ///   <paramref name="algorithmDescription"/> is <see langword="null"/>.
+   ///   - or -
+   ///   <paramref name="alphabet"/> is <see langword="null"/>.
+   /// </exception>
+   /// <exception cref="ArgumentException">
+   ///   <paramref name="algorithmName"/> is <see cref="String.Empty"/> or all
+   ///   whitespace characters.
+   ///   - or. 
+   ///   <paramref name="algorithmDescription"/> is <see cref="String.Empty"/> 
+   ///   or all whitespace characters.
+   /// </exception>
+   /// <exception cref="ArgumentOutOfRangeException">
+   ///   <paramref name="modulus"/> is less than 2.
+   /// </exception>
    public Iso7064HybridSystemAlgorithm(
+      String algorithmName,
+      String algorithmDescription,
       Int32 modulus,
-      String validCharacters)
+      IAlphabet alphabet)
    {
+      _ = algorithmName ?? throw new ArgumentNullException(nameof(algorithmName), Resources.AlgorithmNameIsEmptyMessage);
+      if (String.IsNullOrWhiteSpace(algorithmName))
+      {
+         throw new ArgumentException(Resources.AlgorithmNameIsEmptyMessage, nameof(algorithmName));
+      }
+      _ = algorithmDescription ?? throw new ArgumentNullException(nameof(algorithmDescription), Resources.AlgorithmDescriptionIsEmptyMessage);
+      if (String.IsNullOrWhiteSpace(algorithmDescription))
+      {
+         throw new ArgumentException(Resources.AlgorithmDescriptionIsEmptyMessage, nameof(algorithmDescription));
+      }
       if (modulus < 2)
       {
          throw new ArgumentOutOfRangeException(nameof(modulus), modulus, Resources.Iso7064ModulusOutOfRange);
       }
-      ArgumentException.ThrowIfNullOrEmpty(validCharacters, nameof(validCharacters));
+      _ = alphabet ?? throw new ArgumentNullException(nameof(alphabet), Resources.Iso7046AlphabetIsNull);
 
+      AlgorithmName = algorithmName;
+      AlgorithmDescription = algorithmDescription;
       _modulus = modulus;
+      _alphabet = alphabet;
       _modulusPlus1 = _modulus + 1;
-      _validCharacters = validCharacters;
    }
 
-   /// <summary>
-   ///   Map a character of the value being validated to its integer equivalent.
-   /// </summary>
-   /// <param name="ch">
-   ///   The character to map.
-   /// </param>
-   /// <returns>
-   ///   The integer equivalent of <paramref name="ch"/> or -1 if the character
-   ///   is not valid.
-   /// </returns>
-   public virtual Int32 MapCharacterToNumber(Char ch) => throw new NotImplementedException();
+   /// <inheritdoc/>
+   public String AlgorithmDescription { get; }
 
+   /// <inheritdoc/>
+   public String AlgorithmName { get; }
+
+   /// <inheritdoc/>
    public Boolean TryCalculateCheckDigit(String value, out Char checkDigit)
    {
       checkDigit = CharConstants.NUL;
@@ -49,7 +90,7 @@ public class Iso7064HybridSystemAlgorithm
       var product = _modulus;
       for (var index = 0; index < value.Length; index++)
       {
-         var num = MapCharacterToNumber(value[index]);
+         var num = _alphabet.CharacterToInteger(value[index]);
          if (num == -1)
          {
             return false;
@@ -63,15 +104,12 @@ public class Iso7064HybridSystemAlgorithm
       }
 
       var x = (_modulus - product + 1) % _modulus;
-      if (x < 0 || x > _validCharacters.Length - 1)
-      {
-         return false;
-      }
-      checkDigit = _validCharacters[x];
+      checkDigit = _alphabet.IntegerToCheckCharacter(x);
 
       return true;
    }
 
+   /// <inheritdoc/>
    public Boolean Validate(String value)
    {
       if (String.IsNullOrEmpty(value) || value.Length < 2)
@@ -83,7 +121,7 @@ public class Iso7064HybridSystemAlgorithm
       Int32 num;
       for (var index = 0; index < value.Length - 1; index++)
       {
-         num = MapCharacterToNumber(value[index]);
+         num = _alphabet.CharacterToInteger(value[index]);
          if (num == -1)
          {
             return false;
@@ -96,7 +134,7 @@ public class Iso7064HybridSystemAlgorithm
          product = (product * 2) % _modulusPlus1;
       }
 
-      num = MapCharacterToNumber(value[^1]);
+      num = _alphabet.CharacterToInteger(value[^1]);
       if (num == -1)
       {
          return false;
